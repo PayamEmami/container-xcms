7995fd7423c23462c87c17f4cf0cb673f538ac43
payamemami <payam.emami@medsci.uu.se>
2017-07-22 20:17:41 +0200

    filenameextractor combined with set pheno

--- scripts/setphenotype.r
+++ scripts/setphenotype.r
@@ -14,7 +14,11 @@ phenoDataColumn<-"phenoData"
 
 previousEnv<-NA
 output<-NA
-inputCSV<-NA
+output<-NA
+appendTo<-NA
+appendToName<-"realNames"
+cleanFileName=T
+outputcsv<-NA
 for(arg in args)
 {
   argCase<-strsplit(x = arg,split = "=")[[1]][1]
@@ -37,13 +41,34 @@ for(arg in args)
     
   }
 }
-   if(argCase=="inputCSV")
+if(argCase=="galaxyNames")
   {
-    inputCSV=as.character(value)
+    galaxyNames=as.character(value)
   }
+  if(argCase=="realNames")
+  {
+    realNames=as.character(value)
+  }
+  if(argCase=="cleanFileName")
+  {
+    cleanFileName=as.logical(value)
+  }
+  if(argCase=="appendTo")
+  {
+    appendTo=as.character(value)
+  }
+  if(argCase=="appendToName")
+  {
+    appendToName=as.character(value)
+  }
+
   if(argCase=="phenoDataColumn")
   {
     phenoDataColumn=as.character(value)
+  }
+  if(argCase=="outputcsv")
+  {
+    outputcsv=as.character(value)
   }
   if(argCase=="output")
   {
@@ -52,12 +77,57 @@ for(arg in args)
 }
 
 
-if(is.na(previousEnv) | is.na(output) | is.na(inputCSV)) stop("Both input and output need to be specified!\n")
+if(is.na(previousEnv) | is.na(output)) stop("Both input and output need to be specified!\n")
+
+
+
+
+galaxyNamesTMP<-gsub(pattern = " ",replacement = "",strsplit(x = galaxyNames,split = ",",fixed=T)[[1]],fixed=T)
+realNamesTMP<-gsub(pattern = " ",replacement = "",strsplit(x = realNames,split = ",",fixed=T)[[1]],fixed=T)
+
+galaxyNamesTMP[galaxyNamesTMP!=""]
+realNamesTMP[realNamesTMP!=""]
+
+
+if(cleanFileName)
+  realNamesTMP<-gsub(pattern = "Galaxy.*-\\[|\\].*",replacement = "",x = realNamesTMP)
+
+# can we have different length ?
+if(length(galaxyNamesTMP)!=length(realNamesTMP))
+{
+  stop("galaxy names and real file names have different length!")
+}
+
+# create a dataframe in which galaxynames have their realnames infront of them
+baseNames<-basename(galaxyNamesTMP)
+mappedNames<-data.frame(realNames=realNamesTMP,galaxyNames=baseNames,stringsAsFactors = F)
+mappedNames<-mappedNames[mappedNames[,1]!="",]
+if(!is.na(appendTo))
+{
+  tmpCSVFile<-read.csv(appendTo,stringsAsFactors = F)
+  sortedGalaxyNames<-mappedNames[match(mappedNames[,"realNames"],tmpCSVFile[,appendToName]),"galaxyNames"]
+  sortedGalaxyNamesID<-paste("step_",ncol(tmpCSVFile)+1,sep = "")
+  outCSVFile<-cbind(tmpCSVFile,tmp=sortedGalaxyNames)
+  colnames(outCSVFile)<-c(colnames(tmpCSVFile),sortedGalaxyNamesID)
+  mappedNames<-outCSVFile
+}else
+{
+  colnames(mappedNames)<-c(appendToName,"step_1")
+}
+
+# write the results
+write.csv(x = mappedNames,file = outputcsv,row.names = F)
+
+
+
+
+
+
 
 load(file = previousEnv)
 inputDataSet<-get(varNameForNextStep)
 
-fileNameMap<-read.csv(inputCSV)
+fileNameMap<-mappedNames
 
 fileNameMap$rawFiles<-NA
 # set raw file names
